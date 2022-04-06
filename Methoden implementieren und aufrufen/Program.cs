c24d15e7be15925ad60686a89271e627ced32a33
Florian Reuter <florian.r19@gso.schule.koeln>
2022-04-06 12:55:00 +0200

    Projektdateien hinzufügen.

---
+++ Methoden implementieren und aufrufen/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Methoden_implementieren_und_aufrufen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            short[] zimmernummer = { 45, 23, 3, 32 };
+            string[] vorname = new string[4];
+            vorname[0] = "John";
+            vorname[1] = "Paul";
+            vorname[2] = "George";
+            vorname[3] = "Ringo";
+
+            string[] nachname = new string[4];
+            nachname[0] = "Lennon";
+            nachname[1] = "McCartney";
+            nachname[2] = "Harrison";
+            nachname[3] = "Starr";
+       
+            short[] uebernachtungen = { 1, 4, 12, 3};
+
+            
+
+
+        }
+    }
+}
